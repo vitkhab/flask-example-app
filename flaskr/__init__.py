307bd2d659cc00db0d538b36f98623762cfb97f4
Vitaly Khabarov <vitkhab@gmail.com>
2018-07-25 01:49:05 +0300

    Sleep timer change

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,7 +29,7 @@ def create_app(test_config=None):
 
     @app.route('/hello')
     def hello():
-        time.sleep(1)
+        time.sleep(3)
         return 'Hello, World!'
 
     # register the database commands

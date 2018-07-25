ba65246a5068a42e5096724ea23b587f06a4114c
Vitaly Khabarov <vitkhab@gmail.com>
2018-07-25 19:27:06 +0300

    Add time to start

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
+    time.sleep(10)
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
